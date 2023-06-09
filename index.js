535936bcd152d5044387fbcf4a15dcad86bdec4c
Iqubal905 <iqubal905@gmail.com>
2023-06-10 01:24:13 +0600

    value transfer

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY)
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -144,6 +145,29 @@ app.get('/instructor', async(req, res) =>{
     const result = await instructorCollection.find().toArray();
     res.send(result);
 })
+
+
+
+
+app.post('/create-payment-intent', verifyJWT, async (req, res) => {
+  const { price } = req.body;
+  const amount = parseInt(price * 100);
+  const paymentIntent = await stripe.paymentIntents.create({
+     amount: amount,
+     currency: 'usd',
+     payment_method_types: ['card']
+  });
+
+   res.send({
+      clientSecret: paymentIntent.client_secret
+  })
+})
+
+
+
+
+
+
 
 
 
